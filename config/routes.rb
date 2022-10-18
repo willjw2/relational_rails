b5db33292732ef089f43b6290f24a99622043131
Will Wang <willy.j.wang@gmail.com>
2022-10-18 02:25:01 -0500

    Create team show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/teams', to: 'teams#index'
+  get '/teams/:id', to: 'teams#show'
   get '/players', to: 'players#index'
   get '/players/:id', to: 'players#show'
 end
