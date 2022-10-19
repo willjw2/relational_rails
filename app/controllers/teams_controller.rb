1527029a705bfbdc1cd41243edd9b2640251c481
Will Wang <willy.j.wang@gmail.com>
2022-10-19 04:29:39 -0500

    Create new team page

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,7 +2,16 @@ class TeamsController < ApplicationController
   def index
     @teams = Team.sort_by_recently_created
   end
+
   def show
     @team = Team.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    team = Team.create(name: params[:name], wins: params[:wins], losses: params[:losses], active: params[:active])
+    redirect_to "/teams"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/teams', to: 'teams#index'
+  get '/teams/new', to: 'teams#new'
+  post '/teams', to: 'teams#create'
   get '/teams/:id', to: 'teams#show'
   get '/players', to: 'players#index'
   get '/players/:id', to: 'players#show'
